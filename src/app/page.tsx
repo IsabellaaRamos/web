8b44b00b31a2c4325cdaa92e7ae4afb16b521119
Isabellaaramos <Isabellaramos7@outlook.com>
2025-02-18 18:48:47 -0300

    adiciona variáveis de tema e componente de botão à página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import  { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <main>
       <div className="text-white">Hello world!</div>
+
+      <Button />
 
 
       
