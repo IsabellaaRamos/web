8229c9dee3fdbc860de40ab7caee31e20a633cb9
Isabellaaramos <Isabellaramos7@outlook.com>
2025-02-20 17:13:21 -0300

    adiciona componentes de ícone e imagem à página de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,3 +1,8 @@
 export default function Contato() {
     return <h1>Contato</h1>
 }
+
+import { Radio } from 'lucide-react'
+import Image from 'next/image'
+import logo from '../../assets/logo.svg'
+import { SubscriptionForm } from './subscription-form'
